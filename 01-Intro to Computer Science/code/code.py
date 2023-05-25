904f7c5da9a1df4e45c9db03f01de7f5dbb595c0
Eslam Jekso <jeksogsa@gmail.com>
2023-05-25 17:28:39 +0300

    init repo

---
+++ 01-Intro to Computer Science/code/code.py
@@ -0,0 +1,18 @@
+# This is a comment
+print('Hello world')
+
+
+# second comment
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world') # inline comment
+
+
+
+
+"""
+Test
+Test
+"""
